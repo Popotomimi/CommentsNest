733570719ea65be8c19b1b79c456d6c37a73bc8b
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-03-12 17:52:53 -0300

    update database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+/* DEV 
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
@@ -20,6 +21,37 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: process.env.DB_DATABASE || 'default_database',
       autoLoadEntities: true,
       synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+*/
+
+/* PROD */
+
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CommentsModule } from '../comments/comments.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Permite o uso de variáveis de ambiente globalmente
+    }),
+    CommentsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL, // URL completa fornecida pelo Render
+      ssl: {
+        rejectUnauthorized: false, // Necessário para conexões seguras no Render
+      },
+      autoLoadEntities: true,
+      synchronize: true, // Use com cuidado em produção; ideal desativar depois de criar o schema
     }),
   ],
   controllers: [AppController],
